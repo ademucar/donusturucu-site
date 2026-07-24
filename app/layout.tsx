24649ea45cef6eb63fca2d78029d9d84d101e85a
ademucar <ucaradem317@gmail.com>
2026-07-24 11:43:00 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "PDF ve Resim Aracı",
+  title: "Çeviriyo",
   description: "Hızlı, kolay ve güvenli dönüştürme.",
 };
 
